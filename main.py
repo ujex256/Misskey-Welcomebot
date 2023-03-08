8c2cbc25617b1e30ca6f5126d265befbd075788d
henji243 <105550500+ujex256@users.noreply.github.com>
2023-03-08 18:19:46 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import multiprocessing
+from flask import Flask
+
+from mainbot import bot
+
+app = Flask(__name__)
+
+p = multiprocessing.Process(target=bot)
+p.start()
+
+
+@app.route('/')
+def index():
+    return 'Pong'
+
+
+app.run(host='0.0.0.0', port=81)
