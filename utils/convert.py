130090186ffb63836508cd421d2e1e1bbfa91304
henji243 <105550500+ujex256@users.noreply.github.com>
2023-03-12 19:42:27 +0900

    重複の除去

--- utils/convert.py
+++ utils/convert.py
@@ -54,7 +54,7 @@ if __name__ == "__main__":
     # なんかかっこいい(?)
     for mode in a:
         data = map(lambda x: x + "\n", [functions[mode](word) for word in words])
-        data = list(filter(lambda x: not x.isascii(), data))
+        data = list(filter(lambda x: not (x.isascii() or x in words), data))
         data[-1] = data[-1][:-1]
         with open(f"ngWords_{mode}.txt", "w", encoding="utf8") as f:
             f.writelines(data)
@@ -63,7 +63,7 @@ if __name__ == "__main__":
         data = map(lambda x: x + "\n", ["\n".join(toRomaji(word)) for word in words])
         romaji = []
         for i in data:
-            if i not in romaji and i != "":
+            if (i not in romaji) and (i != "") and (not i in words):
                 romaji.append(i)
         romaji[-1] = romaji[-1][:-1]
         f.writelines(romaji)
