a2d39846b99abc03fb2a6e8305105ddd4e161d20
henji243 <105550500+ujex256@users.noreply.github.com>
2023-03-18 22:05:09 +0900

    flaskテンプレート以外でも動作するようにした

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import multiprocessing
 from flask import Flask
+from replit import web
 
 from src.mainbot import bot
 
-app = Flask(__name__)
-
-p = multiprocessing.Process(target=bot)
-p.start()
-
-
-@app.route('/')
-def index():
-    return 'Pong'
+if __name__ == "__main__":
+    app = Flask(__name__)
+    p = multiprocessing.Process(target=bot)
+    p.start()
 
 
-app.run(host='0.0.0.0', port=81)
+    @app.route('/')
+    def index():
+        return 'Pong'
+    web.run(app)
