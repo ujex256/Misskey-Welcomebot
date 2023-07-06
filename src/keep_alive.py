b67d90cfa0f6733f6477b1fb47342f442bcfa2fd
ujex256 <105550500+ujex256@users.noreply.github.com>
2023-07-06 17:58:35 +0900

    add: 必要に応じてpongサーバーを立てるように変更

--- src/keep_alive.py
+++ src/keep_alive.py
@@ -1,4 +1,21 @@
+from multiprocessing import Process
+from os import getenv
+
+from dotenv import load_dotenv
+from flask import Flask, jsonify
+
 from src.mainbot import bot
 
+app = Flask(__name__)
+
+
+@app.get("/")
+def pong():
+    return jsonify({"message": "Pong!"})
+
+
 if __name__ == "__main__":
+    load_dotenv()
+    if getenv("RUN_SERVER", False):
+        Process(lambda x: app.run()).start()
     bot()
