c99688727bb7278116d6cf20ec471160b62a5eb6
ujex256 <105550500+ujex256@users.noreply.github.com>
2023-07-08 10:37:14 +0900

    feat: 🔊サーバー開始時のログ

--- src/keep_alive.py
+++ src/keep_alive.py
@@ -1,12 +1,20 @@
+import logging
 from multiprocessing import Process
 from os import getenv
 
+import coloredlogs
 from dotenv import load_dotenv
 from flask import Flask, jsonify
 
+import logging_styles
 from mainbot import start_bot
 
+
 app = Flask(__name__)
+
+logger = logging.getLogger(__name__)
+logging_styles.set_default()
+coloredlogs.install(logger=logger)
 
 
 @app.get("/")
@@ -18,4 +26,5 @@ if __name__ == "__main__":
     load_dotenv()
     if getenv("RUN_SERVER", False):
         Process(lambda x: app.run()).start()
+        logging.info("Web server started!")
     start_bot()
