5e1c63194472e5d315003f320d18df90b05177fe
ujex256 <105550500+ujex256@users.noreply.github.com>
2023-07-08 10:21:18 +0900

    delete: 別に分ける必要ない

--- src/ngwords.py
+++ src/ngwords.py
@@ -1,4 +1,3 @@
-import os
 import pathlib
 
 
@@ -6,30 +5,11 @@ class NGWords:
     """
     NGワードのファイルを読み込む
 
-    ngWords.txt
-    ngWords_Hira.txt
-    ngWords_Kana.txt
-    ngWords_Romaji.txt
-    の4つが必要
-
-    initに渡す名前はngWords.txt(ベース)のパスです。
+    initにngワードのテキストファイルのパスを渡してください。
     """
-
-    DEFAULT_NAME = "ngWords.txt"
-    FILENAMES = [
-        "ngWords.txt",
-        "ngWords_Hira.txt",
-        "ngWords_Kana.txt",
-        "ngWords_Romaji.txt",
-    ]
-
     def __init__(self, path) -> None:
         self._path = pathlib.Path(path)
-        if self._path.name != self.DEFAULT_NAME:
-            self.set_default_name(self._path.name)
-        self._ng = {"base": {}, "hira": {}, "kana": {}, "romaji": {}}
-        self._allow = self._ng.copy()
-        self._initialize_words_dict()
+        self._load()
 
     def __getitem__(self, key) -> dict:
         if (key := key.lower()) == "ng":
@@ -39,25 +19,12 @@ class NGWords:
         else:
             raise KeyError('"ng"か"excluded"を指定してください')
 
-    @classmethod
-    def set_default_name(cls, name: str) -> list:
-        cls.DEFAULT_NAME = name
-        cls.FILENAMES = [name]
-        TYPES = ["Hira", "Kana", "Romaji"]
-        s = list(os.path.splitext(name))
-        for i in TYPES:
-            cls.FILENAMES.append(f"{s[0]}_{i}{s[1]}")
-        return cls.FILENAMES
-
-    def _initialize_words_dict(self) -> None:
-        keys = list(self._ng.keys())
-        parent_path = self._path.parent
-        for i in range(4):
-            with parent_path.joinpath(self.FILENAMES[i]).open(encoding="utf8") as f:
-                data = f.read().split("\n")
-            data = [j for j in data if j != ""]
-            self._ng[keys[i]] = {j for j in data if (j[0] != "-") and (j[0] != "#")}
-            self._allow[keys[i]] = {j[1:] for j in data if j[0] == "-"}
+    def _load(self) -> None:
+        with self._path.open() as f:
+            data = f.read().split("\n")
+        data = [j for j in data if j != ""]
+        self._ng = {j for j in data if (j[0] != "-") and (j[0] != "#")}
+        self._allow = {j[1:] for j in data if j[0] == "-"}
 
     def match(self, text) -> bool:
         ng = self.all_ng_words
@@ -72,39 +39,11 @@ class NGWords:
 
     @property
     def all_ng_words(self) -> set:
-        return set().union(*self._ng.values())
+        return self._ng
 
     @property
     def all_excluded_words(self) -> set:
-        return set().union(*self._allow.values())
-
-    @property
-    def ng_word_hiragana(self) -> set:
-        return self._ng["hira"]
-
-    @property
-    def ng_word_katakana(self) -> set:
-        return self._ng["kana"]
-
-    @property
-    def ng_word_romaji(self) -> set:
-        return self._ng["romaji"]
-
-    @property
-    def excluded_word_hiragana(self) -> set:
-        return self._allow["hira"]
-
-    @property
-    def excluded_word_katakana(self) -> set:
-        return self._allow["kana"]
-
-    @property
-    def excluded_word_romaji(self) -> set:
-        return self._allow["romaji"]
-
-
-def set_default_name(name):
-    NGWords.set_default_name(name)
+        return self._allow
 
 
 if __name__ == "__main__":
