61146ed9e0d3e4cc7778c57f96ab90b37fda7f62
fffena <132272963+fffena@users.noreply.github.com>
2023-07-08 13:04:56 +0000

    test: emoji.py

--- tests/test_emoji.py
+++ tests/test_emoji.py
@@ -1,8 +1,20 @@
-import json
-from io import StringIO
+import pytest
 
 from src import emojis
 
 
-def emoji(data):
-    if iseturn emojis.EmojiSet(StringIO(json.dumps(data)))
+messages = ["response.jsonは{'triggers': [], 'others': []}の形にしてください。",
+            "response.jsonのトリガーのキーはkeywordsとemojiにしてください。",
+            "[Errno 2] No such file or directory: 'aaaaaaaa'"]
+
+
+@pytest.mark.parametrize("input,msg",
+                         (["aaaaaaaa", messages[2]],
+                          [[], messages[0]],
+                          [{"d": "a"}, messages[0]],
+                          [{"triggers": [{}], "others": []}, messages[1]]))
+def test_emojiset_error(input, msg):
+    error_cls = FileNotFoundError if isinstance(input, str) else emojis.ConfigJsonError
+    with pytest.raises(error_cls) as e:
+        emojis.EmojiSet(input)
+    assert str(e.value) == msg
