d10135c5af56442da9389d11d20fa93934d47d4f
ujex256 <105550500+ujex256@users.noreply.github.com>
2023-07-09 11:17:25 +0900

    test: can_reply()

--- tests/test_misskey.py
+++ tests/test_misskey.py
@@ -6,7 +6,15 @@ from src import misskey_api as misskey  # NOQA
 
 
 def test_can_renote():
-    with open("tests/test_note.json") as f:
+    with open("tests/test_note_renote.json") as f:
         j = json.load(f)
     for i in j:
         assert misskey.can_renote(i) == i["r"]
+
+
+def test_can_reply():
+    misskey.USERNAME = "youkoso_bot"
+    with open("tests/test_note_reply.json") as f:
+        j = json.load(f)
+    for i in j:
+        assert misskey.can_reply(i) == i["r"]
