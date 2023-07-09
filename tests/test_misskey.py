8d4ef15d8d1b89d05e8128bf1f50d8679bf21c7d
ujex256 <105550500+ujex256@users.noreply.github.com>
2023-07-09 09:50:13 +0900

    test: fix FileNotFoundError

--- tests/test_misskey.py
+++ tests/test_misskey.py
@@ -2,11 +2,11 @@ import json
 import sys
 
 sys.path.append("./src")
-from src import misskey_api as misskey
+from src import misskey_api as misskey  # NOQA
 
 
 def test_can_renote():
-    with open("test_note.json") as f:
+    with open("tests/test_note.json") as f:
         j = json.load(f)
     for i in j:
         assert misskey.can_renote(i) == i["r"]
