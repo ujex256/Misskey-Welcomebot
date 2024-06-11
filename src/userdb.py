760813caa2fc9419afbc4582779ad1f5a94aabdd
ujex256 <105550500+ujex256@users.noreply.github.com>
2024-06-11 14:48:38 +0900

    feat: 検索機能の追加とユーザー名を保存する

--- src/userdb.py
+++ src/userdb.py
@@ -1,14 +1,14 @@
 import asyncio
 
 from pydantic import PastDatetime
-from aredis_om import HashModel, get_redis_connection, Field
+from aredis_om import HashModel, Migrator, get_redis_connection, Field, NotFoundError
 
 from datetime import datetime
 
 
 class UserInfo(HashModel):
     user_id: str = Field(primary_key=True)
-    user_name: str
+    user_name: str = Field(index=True)
     last_received_date: PastDatetime
 
     class Meta:
@@ -19,26 +19,38 @@ class UserInfo(HashModel):
 class UserDB:
     def __init__(self, redis_url: str) -> None:
         self._db_url = redis_url
-        UserInfo.Meta.database = get_redis_connection(url=self._db_url, decode_responses=True)  # type: ignore
+        UserInfo.Meta.database = get_redis_connection(url=self._db_url)  # type: ignore
 
     async def get_all_users(self) -> list[UserInfo]:
         all_pks = await UserInfo.all_pks()
         return await asyncio.gather(*[UserInfo.get(i) async for i in all_pks])
 
-    async def get_user(self, user_id: str) -> UserInfo:
-        return await UserInfo.get(user_id)
+    async def get_user_by_id(self, user_id: str) -> UserInfo | None:
+        try:
+            return await UserInfo.get(user_id)
+        except NotFoundError:
+            return None
+
+    async def get_user_by_name(self, aas: str) -> UserInfo | None:
+        await self._migrate()
+        a = UserInfo.find(UserInfo.user_name == aas)
+        try:
+            return await a.first()  # type: ignore
+        except NotFoundError:
+            return None
 
     async def add_user(self, user_id: str, username: str):
         await UserInfo(
-            user_id=user_id,
-            user_name=username,
-            last_received_date=datetime.now()
+            user_id=user_id, user_name=username, last_received_date=datetime.now()
         ).save()
+
+    async def _migrate(self):
+        await Migrator().run()
 
 
 if __name__ == "__main__":
     import asyncio
-    from environs import Settings
 
-    db = UserDB(str(Settings().db_url))
-    print(asyncio.run(db.get_all_users()))
+    DB_URL = "redis://localhost:6379"
+    db = UserDB(DB_URL)
+    print(type(asyncio.run(db.get_user_by_name("ffdi"))))
