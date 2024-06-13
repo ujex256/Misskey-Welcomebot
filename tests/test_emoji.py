b3d7e0c7cb330430a65c593bcd717af6f431175c
ujex256 <105550500+ujex256@users.noreply.github.com>
2024-06-13 17:13:40 +0900

    test: pytestで直接インポートできるように from src import ...を直した

--- tests/test_emoji.py
+++ tests/test_emoji.py
@@ -1,20 +1,22 @@
 import pytest
-
-from src import emojis
+import emojis
 
 
 messages = ["response.jsonは{'triggers': [], 'others': []}の形にしてください。",
             "response.jsonのトリガーのキーはkeywordsとemojiにしてください。",
-            "[Errno 2] No such file or directory: 'aaaaaaaa'"]
+            "[Errno 2] No such file or directory: 'aaaaaaaa'",
+            "Invalid type for path: <class 'list'>. Expected str, PathLike, or NoneType."
+]
 
 
 @pytest.mark.parametrize("input,msg",
                          (["aaaaaaaa", messages[2]],
-                          [[], messages[0]],
+                          [[], messages[3]],
                           [{"d": "a"}, messages[0]],
                           [{"triggers": [{}], "others": []}, messages[1]]))
 def test_emojiset_error(input, msg):
     error_cls = FileNotFoundError if isinstance(input, str) else emojis.ConfigJsonError
     with pytest.raises(error_cls) as e:
         emojis.EmojiSet(input)
+    print(msg)
     assert str(e.value) == msg
--- tests/test_ngwords.py
+++ tests/test_ngwords.py
@@ -1,4 +1,4 @@
-from src import ngwords
+import ngwords
 
 
 ng = {"r-18", "荒らす", "twitter", "ワード"}
