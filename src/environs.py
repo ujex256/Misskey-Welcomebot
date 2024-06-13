8c3da9904074d83aad7e70f594477bb672291cdd
ujex256 <105550500+ujex256@users.noreply.github.com>
2024-06-13 20:31:30 +0900

    feat: 環境変数にpongサーバーの設定を追加

--- src/environs.py
+++ src/environs.py
@@ -1,7 +1,9 @@
+from ipaddress import IPv4Address
 from pathlib import Path
 from typing import Literal, Optional
 
-from pydantic import DirectoryPath, RedisDsn, Field, model_validator
+from pydantic import DirectoryPath, Field, model_validator
+from pydantic.networks import IPvAnyAddress, RedisDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -18,9 +20,14 @@ class Settings(DotenvSettings):
 
     config_dir: DirectoryPath = Path("./config")
     run_server: bool = False
+    server_host: IPvAnyAddress = IPv4Address("0.0.0.0")  # type: ignore
+    server_port: int = 8000
 
     @model_validator(mode="after")
     def required_when_redis(self):
         if self.db_url is None and self.db_type == "redis":
             raise ValueError("DB_URL is required when DB_TYPE is redis")
+
+        if not (self.server_port > 0 and self.server_port < 65536):
+            raise ValueError("SERVER_PORT must be between 1 and 65535")
         return self
--- src/keep_alive.py
+++ src/keep_alive.py
@@ -3,10 +3,9 @@ from multiprocessing import Process
 
 from flask import Flask, jsonify
 
+import environs
 import logging_styles
 import mainbot
-import environs
-
 
 app = Flask("app")
 
@@ -18,13 +17,13 @@ def pong():
     return jsonify({"message": "Pong!"})
 
 
-def run_server():
-    app.run(host="0.0.0.0", port=8080)
+def run_server(host: str, port: int):
+    app.run(host=host, port=port)
 
 
 if __name__ == "__main__":
     config = environs.Settings()
     if config.run_server:
-        Process(target=run_server).start()
+        Process(target=run_server, args=(str(config.server_host), config.server_port)).start()
         logger.info("Web server started!")
     asyncio.run(mainbot.Bot(config).start_bot())
