140acb91424d0bc8539747bc344679ac6ee477c4
ujex256 <105550500+ujex256@users.noreply.github.com>
2024-06-13 17:26:00 +0900

    fix(test): 正しい判定ができていない 例外などを個別に書く

--- tests/test_emoji.py
+++ tests/test_emoji.py
@@ -1,22 +1,18 @@
 import pytest
+
 import emojis
 
-
-messages = ["response.jsonは{'triggers': [], 'others': []}の形にしてください。",
-            "response.jsonのトリガーのキーはkeywordsとemojiにしてください。",
-            "[Errno 2] No such file or directory: 'aaaaaaaa'",
-            "Invalid type for path: <class 'list'>. Expected str, PathLike, or NoneType."
+inputs = [
+    ["aaa", "[Errno 2] No such file or directory: 'aaa'", FileNotFoundError],
+    [[], "Invalid type for path: <class 'list'>. Expected str, PathLike, or NoneType.", TypeError],
+    [{"a": "b"}, "response.jsonは{'triggers': [], 'others': []}の形にしてください。", emojis.ConfigJsonError],
+    [{"triggers": [{}], "others": []}, "response.jsonのトリガーのキーはkeywordsとemojiにしてください。", emojis.ConfigJsonError]
 ]
 
 
-@pytest.mark.parametrize("input,msg",
-                         (["aaaaaaaa", messages[2]],
-                          [[], messages[3]],
-                          [{"d": "a"}, messages[0]],
-                          [{"triggers": [{}], "others": []}, messages[1]]))
-def test_emojiset_error(input, msg):
-    error_cls = FileNotFoundError if isinstance(input, str) else emojis.ConfigJsonError
-    with pytest.raises(error_cls) as e:
+@pytest.mark.parametrize("input,msg,exception", inputs)
+def test_emojiset_error(input, msg, exception):
+    with pytest.raises(exception) as e:
         emojis.EmojiSet(input)
     print(msg)
     assert str(e.value) == msg
