a546c1dab5b6e366ab4799ed648055f3926df9f2
ujex256 <105550500+ujex256@users.noreply.github.com>
2024-07-15 10:48:44 +0900

    lint

--- tests/test_emoji.py
+++ tests/test_emoji.py
@@ -4,13 +4,25 @@ import emojis
 
 inputs = [
     ["aaa", "[Errno 2] No such file or directory: 'aaa'", FileNotFoundError],
-    [[], "Invalid type for path: <class 'list'>. Expected str, PathLike, or NoneType.", TypeError],
-    [{"a": "b"}, "response.jsonは{'triggers': [], 'others': []}の形にしてください。", emojis.ConfigJsonError],
-    [{"triggers": [{}], "others": []}, "response.jsonのトリガーのキーはkeywordsとemojiにしてください。", emojis.ConfigJsonError]
+    [
+        [],
+        "Invalid type for path: <class 'list'>. Expected str, PathLike, or NoneType.",
+        TypeError,
+    ],
+    [
+        {"a": "b"},
+        "response.jsonは{'triggers': [], 'others': []}の形にしてください。",
+        emojis.ConfigJsonError,
+    ],
+    [
+        {"triggers": [{}], "others": []},
+        "response.jsonのトリガーのキーはkeywordsとemojiにしてください。",
+        emojis.ConfigJsonError,
+    ],
 ]
 
 
-@pytest.mark.parametrize("input,msg,exception", inputs)
+@pytest.mark.parametrize("input, msg, exception", inputs)
 def test_emojiset_error(input, msg, exception):
     with pytest.raises(exception) as e:
         emojis.EmojiSet(input)
