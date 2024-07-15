29c0be43f82c772a94ad5cbdc726c95e2e118eb6
ujex256 <105550500+ujex256@users.noreply.github.com>
2024-07-15 23:05:49 +0900

    tests: 環境変数クラスのテスト

---
+++ tests/test_environs.py
@@ -0,0 +1,39 @@
+# pyright: reportCallIssue=false
+import pytest
+from pydantic import ValidationError
+from typing import Type
+
+from environs import Settings
+
+
+default_kwargs = {
+    "_env_file": None,  # Don't load .env file
+    "host": "dummy",
+    "secret_token": "dummy",
+}
+
+
+def test_required():
+    with pytest.raises(ValidationError) as e:
+        Settings(_env_file=None)
+        assert e.value.error_count() == 2
+        assert [i["loc"][0] for i in e.value.errors()] == ["host", "secret_token"]
+
+
+@pytest.mark.parametrize(
+    "url, exp",
+    [
+        [None, ValueError],
+        ["non-redis-url", ValidationError],
+        ["redis://localhost:6379", None],
+    ],
+)
+def test_redis_db(url: str | None, exp: Type[Exception] | None):
+    kwargs = default_kwargs.copy()
+    kwargs["db_type"] = "redis"
+    kwargs["db_url"] = url
+    if exp is not None:
+        with pytest.raises(exp):
+            Settings(**kwargs)
+    else:
+        Settings(**kwargs)
