1f3ddc7f71ebce0021632b13a9a6400fb6fe859c
ujex256 <105550500+ujex256@users.noreply.github.com>
2024-07-16 15:14:28 +0900

    tests: utils.pyへの対応

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,13 +1,13 @@
 import json
 
-import misskey_api as misskey  # NOQA
+import utils
 
 
 def test_can_renote():
     with open("tests/test_note_renote.json") as f:
         j = json.load(f)
     for i in j:
-        assert misskey.can_renote(i) == i["r"]
+        assert utils.can_renote(i) == i["r"]
 
 
 def test_can_reply():
@@ -15,4 +15,4 @@ def test_can_reply():
     with open("tests/test_note_reply.json") as f:
         j = json.load(f)
     for i in j:
-        assert misskey.can_reply(i, username) == i["r"]
+        assert utils.can_reply(i, username) == i["r"]
